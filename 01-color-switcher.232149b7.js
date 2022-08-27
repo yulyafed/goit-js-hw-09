86b263b75073f38336a0a93ffda61147dfe18ec7
yulyafed <99392460+yulyafed@users.noreply.github.com>
2022-08-27 01:07:02 +0000

    Deploying to gh-pages from @ yulyafed/goit-js-hw-09@c352ef9a990c1f56363b2dfc58b128c210fbb776 🚀

---
+++ 01-color-switcher.232149b7.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,intervalId=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(intervalId),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.232149b7.js.map
